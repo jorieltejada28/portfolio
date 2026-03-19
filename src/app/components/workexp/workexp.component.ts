d8c10fdfeb5f133374edbb741bb8ee6c4a8b6b52
jorieltejada28 <jorieltejada84@gmail.com>
2026-03-20 00:28:12 +0800

    commit

---
+++ src/app/components/workexp/workexp.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workexp',
+  imports: [],
+  templateUrl: './workexp.component.html',
+  styleUrl: './workexp.component.css',
+})
+export class WorkexpComponent {
+  experiences = [
+    {
+      company: 'Department of Social Welfare and Development (DSWD)',
+      role: 'Full Stack Developer',
+      period: '2025 - Present',
+      description: 'Contributing to the development and maintenance of government systems that support public service delivery. Focusing on scalable web platforms and mobile applications.',
+      technologies: ['Laravel', 'Angular', 'Node.js', 'TypeScript']
+    }
+  ];
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainComponent } from '../../layouts/main/main.component';
 import { ProfileComponent } from '../../components/profile/profile.component';
+import { WorkexpComponent } from '../../components/workexp/workexp.component';
 
 @Component({
   selector: 'app-home',
-  imports: [MainComponent, ProfileComponent],
+  imports: [MainComponent, ProfileComponent, WorkexpComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
