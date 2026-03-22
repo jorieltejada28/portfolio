e3a48106c75b5ace701929d02bbbdb88bf289b6d
jorieltejada28 <jorieltejada84@gmail.com>
2026-03-22 12:15:42 +0800

    commit

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterLink ,RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
@@ -10,6 +12,11 @@ export class NavbarComponent {
   isMenuHidden = true;
   title = "Portfolio";
 
+  links = [
+    { title: 'Home', path: '/' },
+    { title: 'Projects', path: '/projects' },
+    { title: 'About', path: '/about' },
+  ]
    toggleMenu() {
     this.isMenuHidden = !this.isMenuHidden;
   }
